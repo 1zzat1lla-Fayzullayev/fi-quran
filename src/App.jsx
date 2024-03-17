3de1d145cdb1190c9be45233f15fe5b2a0cb8ce9
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-03-18 01:52:09 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,35 @@ import UzbTranslate from "./pages/UzbTranslate";
 import Footer from "./components/Footer";
 import Swap from "./shared/ui/Swap";
 
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updatePosition = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", updatePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updatePosition);
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className="cursor-dot bg-base-content"
+        style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      ></div>
+      <div
+        className="cursor-outline"
+        style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      ></div>
+    </>
+  );
+};
+
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [theme, setTheme] = useState(
@@ -43,6 +72,7 @@ const App = () => {
 
   return (
     <div>
+      <Cursor />
       <div className="fixed top-[90%] right-3 z-[1000]">
         <Swap handleToggle={handleToggle} />
       </div>
